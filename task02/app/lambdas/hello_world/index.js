a002931cba81cda445fcc18ce8bbb6019e26634f
dzianis-l <leadzian213@gmail.com>
2024-05-14 17:58:26 +0300

    task02: 2.0.1 - lambda function url, no api gateway

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,18 +1,30 @@
 var PATH = "/hello";
 var METHOD = "GET";
 
+function logging(...args) {
+    console.log("logging", JSON.stringify(args));
+}
+
 exports.handler = async (event) => {
     const { method, path } = event.requestContext?.http || {};
+
+    logging(event.requestContext);
 
     if (path === PATH && method === METHOD) {
         return {
             statusCode: 200,
-            message: "Hello from Lambda",
+            body: {
+                statusCode: 200,
+                message: "Hello from Lambda",
+            },
         };
     } else {
         return {
             statusCode: 400,
-            message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
+            body: {
+                statusCode: 400,
+                message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
+            },
         };
     }
 };
