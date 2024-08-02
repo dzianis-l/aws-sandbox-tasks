a5f9977d5c3b29a7e580840b455905a9660b9e07
dzianis-l <leadzian213@gmail.com>
2024-08-02 23:18:45 +0300

    task03: 3.0.0 - build + lambda

---
+++ task03/app/lambdas/hello_world/index.js
@@ -0,0 +1,9 @@
+exports.handler = async (event) => {
+    return {
+        statusCode: 200,
+        body: {
+            statusCode: 200,
+            message: "Hello from Lambda",
+        },
+    };
+};
