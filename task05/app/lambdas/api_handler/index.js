8ea409ba2e50be0aaa7150371c28d67089556a1e
dzianis-l <leadzian213@gmail.com>
2024-08-07 11:00:16 +0300

    task05: 5.0.0 - feat: dynamodb, λ, apigateway

---
+++ task05/app/lambdas/api_handler/index.js
@@ -0,0 +1,54 @@
+const AWS = require("aws-sdk");
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const { v4: uuidv4 } = require("uuid");
+
+const log = {
+    error: console.error.bind(console),
+    info: console.log.bind(console),
+};
+
+exports.handler = async (event) => {
+    log.info(`Event: ${JSON.stringify(event)}`);
+    log.info(`TARGET_TABLE: ${tableName}`);
+    
+    const tableName = process.env.TARGET_TABLE;
+    const now = new Date();
+    const isoFormat = now.toISOString();
+
+    let item;
+
+    try {
+        item = {
+            id: uuidv4(),
+            principalId: event.principalId,
+            createdAt: isoFormat,
+            body: event.content,
+        };
+    } catch (error) {
+        log.error(error);
+        const body = event.body;
+        const data = JSON.parse(body);
+
+        item = {
+            id: uuidv4(),
+            principalId: data.principalId,
+            createdAt: isoFormat,
+            body: data,
+        };
+        event = data;
+    }
+
+    const response = await dynamodb
+        .put({
+            TableName: tableName,
+            Item: item,
+        })
+        .promise();
+
+    log.info(`DynamoDB put_item response: ${JSON.stringify(response)}`);
+
+    return {
+        statusCode: 201,
+        event,
+    };
+};
